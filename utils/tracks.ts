a0ac7e4e9bd8078e19358bd2e3925b28cd45fea1
Kevinectv <34521425+kevinectv@users.noreply.github.com>
2025-06-09 18:44:39 -0500

    Add files via upload

--- utils/tracks.ts
+++ utils/tracks.ts
@@ -1,1 +1,9 @@
-
+export const tracks = [
+  {
+    id: '1',
+    name: 'Backyard Lofi',
+    artist: 'Lofium',
+    audio_url: 'https://cdn.jsdelivr.net/gh/kevinectv/musica-relajante/audio/lofi-backyard.mp3',
+    cover_url: 'https://cdn.jsdelivr.net/gh/kevinectv/musica-relajante/audio/lofi-backyard.jpg',
+  }
+];
